d48737e2a3bbad2c97527d0e41d27db6e983386f
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-16 19:49:09 +0530

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x + y
+
+def sub(x,y):
+    return x - y
+
+def mul(x,y):
+    return x * y
+
+def div(x,y):
+    return x / y
+
+num1 = int(input("Enter the First Number: ")) 
+num2 = int(input("Enter the Second Number: ")) 
+
+print("Addtion" , add(num1 , num2))
+print("Substraction" , sub(num1 , num2))
+print("Mutiplication" , mul(num1 , num2))
+print("Division" , div(num1 , num2))
+ 
