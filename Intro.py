84ecc54f5c16f02eed7da0642950a81b925533e0
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-16 19:18:54 +0530

    Introduction to yourself

---
+++ Intro.py
@@ -0,0 +1,10 @@
+def intro(name):
+    print("Hi my name is: " , name)
+username = input("Enter Your Name: ") 
+
+intro(username)
+
+# def intro(name):
+#     print("Hi my name is: " , name)
+
+# intro("Hanzla")
